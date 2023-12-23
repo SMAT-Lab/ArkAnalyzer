7116ae289e4649b21a62441ea8bfe92e9b4a9b7d
yyz <2842418411@qq.com>
2023-12-23 20:05:52 +0800

    update CHA, RTA based on CFG

--- callgraph/ClassHierarchyAnalysisAlgorithm.ts
+++ callgraph/ClassHierarchyAnalysisAlgorithm.ts
@@ -1,5 +1,7 @@
 import {AbstractCallGraphAlgorithm} from "./AbstractCallGraphAlgorithm";
-import {MethodSignature} from "../core/ArkSignature";
+import {ClassSignature, MethodSignature, MethodSubSignature} from "../core/ArkSignature";
+import {ArkClass} from "../core/ArkClass";
+import {ArkFile} from "../core/ArkFile";
 
 class ClassHierarchyAnalysisAlgorithm extends AbstractCallGraphAlgorithm {
     protected resolveCall(sourceMethodSignature: MethodSignature, invokeExpression): MethodSignature[] {
@@ -7,24 +9,43 @@ class ClassHierarchyAnalysisAlgorithm extends AbstractCallGraphAlgorithm {
         let concreteMethod : MethodSignature;
         let callTargetMethods : MethodSignature[];
 
+        // concreteMethod = cfg.getMethodSignature(invokeExpression);
+
         // if (concreteMethod == null ||
-        //     // concreteMethod.isStatic()) {
+        //     concreteMethod.isStatic()) {
         //     // 若调用函数为静态方法直接返回签名
-        //     // callTargetMethods.push(concreteMethod)
-        //     // return callTargetMethods
+        //     callTargetMethods.push(concreteMethod)
+        //     return callTargetMethods
         // } else {
         //     // TODO: 根据获取的方法签名获取可能的全部调用目标方法，方法签名需要包括所在类
         //     callTargetMethods = this.resolveAllCallTargets(concreteMethod)
-        //     if (!concreteMethod.isAbstract()) {
-        //         callTargetMethods.push(concreteMethod)
-        //     }
         //     return callTargetMethods
         // }
         return []
     }
 
     protected resolveAllCallTargets(targetMethodSignature: MethodSignature): MethodSignature[] {
-        // TODO: 对于给出方法签名，获取方法所在的类，并该类的所有子类，是否存在继承的方法
-        return [];
+        // TODO: 对于给出方法签名，获取方法所在的类，并同时获取该类的所有子类，是否存在继承的方法
+        // let targetClasses: ClassSignature[];
+        // let methodSignature: MethodSignature[] = [];
+        // targetClasses = []
+        // for (let targetClass of targetClasses) {
+        //     // TODO: 获取到类的信息
+        //     let arkClass = arkFile.getClass(targetClass)
+        //     let methods = arkClass.getMethods()
+        //
+        //     for (let method of methods) {
+        //         // TODO: 后续需要考虑比较使用subSignature还是直接名字比较
+        //         if (method.methodSubSignature === targetMethodSignature.methodSubSignature) {
+        //             methodSignature.push(method.methodSignature)
+        //         }
+        //     }
+        // }
+        // return methodSignature;
+        return []
+    }
+
+    protected preProcessMethod(methodSignature: MethodSignature): void {
+        //do nothing
     }
 }
--- callgraph/RapidTypeAnalysisAlgorithm.ts
+++ callgraph/RapidTypeAnalysisAlgorithm.ts
@@ -1,8 +1,12 @@
 import {AbstractCallGraphAlgorithm} from "./AbstractCallGraphAlgorithm";
-import {ClassSignature, MethodSignature} from "../core/ArkSignature";
+import {ClassSignature, MethodSignature, MethodSubSignature} from "../core/ArkSignature";
+import {CFG} from "../core/base/Cfg";
 
+type Tuple = [MethodSignature, MethodSignature];
 class RapidTypeAnalysisAlgorithm extends AbstractCallGraphAlgorithm {
-    private instantiatedClasses : Set<ClassSignature>
+    private instancedClasses : Set<ClassSignature>
+    // TODO: ignoredCalls未做重复检查
+    private ignoredCalls : Map<ClassSignature, Tuple[]>
     protected resolveCall(sourceMethodSignature: MethodSignature, invokeExpression): MethodSignature[] {
         // TODO: 根据调用语句获取具体方法，函数签名
         let concreteMethod : MethodSignature;
@@ -14,17 +18,87 @@ class RapidTypeAnalysisAlgorithm extends AbstractCallGraphAlgorithm {
         //     callTargetMethods.push(concreteMethod)
         //     return callTargetMethods
         // } else {
-        //     // TODO: 获取可能的全部调用目标方法
-        //     callTargetMethods = this.resolveAllCallTargets()
-        //     if (!concreteMethod.isAbstract()) {
-        //         callTargetMethods.push(concreteMethod)
+        //     if (this.instancedClasses.has(sourceMethodSignature.arkClass)) {
+        //         // TODO: 获取可能的全部调用目标方法
+        //         callTargetMethods = this.resolveAllCallTargets(sourceMethodSignature, concreteMethod)
+        //         if (!concreteMethod.isAbstract()) {
+        //             callTargetMethods.push(concreteMethod)
+        //         }
+        //     } else {
+        //         // TODO: 未实例化需要加入到ignoredCalls中
         //     }
         //     return callTargetMethods
         // }
         return []
     }
 
-    protected resolveAllCallTargets(): MethodSignature[] {
-        return [];
+    protected resolveAllCallTargets(sourceMethodSignature, targetMethodSignature: MethodSignature): MethodSignature[] {
+        // TODO: 对于给出方法签名，获取方法所在的类，并该类的所有子类，是否存在继承的方法
+        let targetClasses: ClassSignature[];
+        let methodSignature: MethodSignature[] = [];
+        targetClasses = []
+        for (let targetClass of targetClasses) {
+            // TODO: 获取到类的信息
+            // let arkClass = arkFile.getClass(targetClass)
+            // let methods = arkClass.getMethods()
+
+            // for (let method of methods) {
+            //     // TODO: 后续需要考虑比较使用subSignature还是直接名字比较
+            //     if (method.methodSubSignature === targetMethodSignature.methodSubSignature) {
+            //         if (this.instancedClasses.has(targetClass)) {
+            //             methodSignature.push(method.methodSignature)
+            //         } else {
+            //             this.saveIgnoredCalls(sourceMethodSignature, method)
+            //         }
+            //     }
+            // }
+        }
+        return methodSignature;
+    }
+
+    protected preProcessMethod(methodSignature: MethodSignature): void {
+        let newInstancedClasses = this.collectInstantiatedClassesInMethod(methodSignature)
+        for (let newInstancedClass of newInstancedClasses) {
+            // Soot 中前处理没看明白，先写个简单版本
+            // 从未实例化方法调用中检查是否有新的实例，并重新加入到calls中
+            if (this.ignoredCalls.has(newInstancedClass)) {
+                let reinsertEdge = this.ignoredCalls.get(newInstancedClass)
+                for (let edge of reinsertEdge) {
+                    this.addCall(edge[0], edge[1]);
+                    if (!this.hasMethod(edge[1])) {
+                        this.signatureManager.addToWorkList(edge[1]);
+                    }
+                }
+                this.ignoredCalls.delete(newInstancedClass)
+            }
+        }
+    }
+
+    // 获取方法中新创建的类对象
+    protected collectInstantiatedClassesInMethod(methodSignature: MethodSignature): ClassSignature[] {
+        let cfg : CFG;
+        // let instancedClass, newInstancedClass: ClassSignature[]
+        // TODO: 获取cfg中的语句
+        // for (let stmt of cfg.?) {
+        //     if (stmt.kindProperty == JAssignStmt) {
+        //         let classSignature = getClassSignature();
+        //         if (this.instancedClasses.has(classSignature)) {
+        //
+        //         } else {
+        //             newInstancedClass.push(classSignature)
+        //         }
+        //     }
+        // }
+        // return newInstancedClass
+        return []
+    }
+
+    protected saveIgnoredCalls(sourceMethodSignature, targetMethodSignature: MethodSignature) {
+        let notInstancedClass = targetMethodSignature.arkClass
+        if (this.ignoredCalls.has(notInstancedClass)) {
+            this.ignoredCalls.get(notInstancedClass).push([sourceMethodSignature, targetMethodSignature])
+        } else {
+            this.ignoredCalls.set(notInstancedClass, [sourceMethodSignature, targetMethodSignature])
+        }
     }
 }
