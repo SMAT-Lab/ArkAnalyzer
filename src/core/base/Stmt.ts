88bdcacad2770f6c92d288b3d2892fb06d38b45c
Li Li <lilicoding@gmail.com>
2024-01-18 11:37:09 +0800

    change defs to def

--- src/core/base/Stmt.ts
+++ src/core/base/Stmt.ts
@@ -24,12 +24,12 @@ export class Stmt {
         this.uses.push(use);
     }
 
-    public getdefs(): Value[] {
-        return this.defs;
+    public getDef(): Value {
+        return this.def;
     }
 
-    public addDef(def: Value): void {
-        this.defs.push(def);
+    public setDef(def: Value): void {
+        this.def = def;
     }
 
     public getValueVersion(value: Value): string | undefined {
@@ -83,10 +83,8 @@ export class Stmt {
                 return true;
             }
         }
-        for (const def of this.defs) {
-            if (def instanceof ArkArrayRef) {
-                return true;
-            }
+        if (this.def instanceof ArkArrayRef) {
+            return true;
         }
         return false;
     }
@@ -97,11 +95,11 @@ export class Stmt {
                 return use as ArkArrayRef;
             }
         }
-        for (const def of this.defs) {
-            if (def instanceof ArkArrayRef) {
-                return def as ArkArrayRef;
-            }
+
+        if (this.def instanceof ArkArrayRef) {
+            return undefined;
         }
+
         return undefined;
     }
 
@@ -111,10 +109,9 @@ export class Stmt {
                 return true;
             }
         }
-        for (const def of this.defs) {
-            if (def instanceof ArkFieldRef) {
-                return true;
-            }
+
+        if (this.def instanceof ArkFieldRef) {
+            return true;
         }
         return false;
     }
@@ -125,10 +122,8 @@ export class Stmt {
                 return use as ArkFieldRef;
             }
         }
-        for (const def of this.defs) {
-            if (def instanceof ArkFieldRef) {
-                return def as ArkFieldRef;
-            }
+        if (this.def instanceof ArkFieldRef) {
+            return undefined;
         }
         return undefined;
     }
@@ -163,7 +158,7 @@ export class ArkAssignStmt extends Stmt {
         super();
         this.leftOp = leftOp;
         this.rightOp = rightOp;
-        this.addDef(leftOp);
+        this.setDef(leftOp);
         this.addUse(rightOp);
         for (const use of rightOp.getUses()) {
             this.addUse(use);
