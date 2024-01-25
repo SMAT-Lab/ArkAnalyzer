7c9e1546cd29d2664770d09d3e5dfb508292787d
19612 <1961289751@qq.com>
2024-01-25 20:03:14 +0800

    test

--- tests/resources/cfg/main.ts
+++ tests/resources/cfg/main.ts
@@ -1,16 +1,20 @@
-// function fun(){let i=0;
-// if(i<1){
-//     i++;
-// }
-// else{
-//     i--
-// }
-// console.log(i)}
-
-let i:number;
-if(xx){
-    i=1;
+let i=0;
+if(i<1){
+    i++;
 }
 else{
-    i=2;
+    i--
 }
+console.log(i)
+
+// let p=new personalbar();
+// x=p.y;
+// p.y=t;
+// y=p.y;
+
+// let a:number[]=[1,2,3];
+// let a1=1;
+// a.push(a1);
+// x=a[a1];
+// a[1]=0;
+// x=a[a1];
