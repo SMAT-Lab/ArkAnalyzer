b58da48517c38bc415d5de79ae3e902180284d79
19612 <1961289751@qq.com>
2024-01-27 16:02:21 +0800

    cfg update

--- tests/resources/type/dir1/dir2/test.ts
+++ tests/resources/type/dir1/dir2/test.ts
@@ -1,5 +1,8 @@
 import { type3 } from "./type3";
 import { type0 } from "../../type0";
+import { type1,type2 } from "../type1";
 
 let t0=new type0();
+let t1=new type1();
+let t2=new type2();
 let t3=new type3();
