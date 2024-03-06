262616fad4bf841b0dcf63b692e233027a95f48b
19612 <1961289751@qq.com>
2024-03-06 17:55:46 +0800

    UnclearReferenceType update

--- tests/resources/typeInference/project/Index.ts
+++ tests/resources/typeInference/project/Index.ts
@@ -1,21 +1,16 @@
-import animator, { AnimatorResult } from '@ohos.animator';
-let backAnimator: AnimatorResult = animator.createAnimator({
-    duration: 2000,
-    easing: "ease",
-    delay: 0,
-    fill: "forwards",
-    direction: "normal",
-    iterations: 1,
-    begin: 100,
-    end: 200
-});
-backAnimator.onfinish();
-backAnimator.onrepeat = () => {
-    console.info('backAnimator repeat');
-};
-backAnimator.oncancel = () => {
-    console.info('backAnimator cancel');
-};
-backAnimator.onframe = (value: number) => {
-    console.info('backAnimator cancel');
-};
+import media from '@ohos.multimedia.media';
+const TAG: string = '[AVPlayerModel]';
+export default class AVPlayerModel {
+    private avPlayer: media.AVPlayer;
+    private context;
+    constructor(context) {
+        this.context = context;
+    }
+    func(){
+        this.avPlayer.on('seekDone', (seekDoneTime) => {
+        Logger.info(TAG, `AVPlayer seek succeeded, seek time is ${seekDoneTime}`);
+    });
+    }
+
+    
+}
