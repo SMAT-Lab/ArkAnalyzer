6c185c4bed177c69ed8bf5fd9f23437cee5c1968
19612 <1961289751@qq.com>
2024-03-07 19:22:53 +0800

    import type reference

--- src/core/dataflow/Fact.ts
+++ src/core/dataflow/Fact.ts
@@ -1,6 +1,7 @@
 import { Value } from "../base/Value";
-
+import { Stmt } from "../base/Stmt";
 
 export class Fact {
     values: Set<Value>;
+    valueMap: Map<Value, Stmt>  // 用最近的def代表value的值
 }
--- tests/arkts2tsMapLineTest.ts
+++ tests/arkts2tsMapLineTest.ts
@@ -1,36 +1,41 @@
-// import * as sourceMap from 'source-map';
+import * as sourceMap from 'source-map';
 
-// // const sourceMap = require('source-map')
-// const tsmap = require('fs').readFileSync('D:/11study/openharmony/ArkUIWeChat/resultTsDir/entry/src/main/ets/model/CommonStyle.ts.map', 'utf-8')
+// const sourceMap = require('source-map')
+const tsmap = require('fs').readFileSync('D:/11study/openharmony/ArkUIWeChat/resultTsDir/entry/src/main/ets/model/CommonStyle.ts.map', 'utf-8')
 
-// let consumer =await new sourceMap.SourceMapConsumer(tsmap)
-// const compressedPosition = {
-//     line: 1,
-//     column: 0
-// };
+async function func() {
+    let consumer =await new sourceMap.SourceMapConsumer(tsmap)
+    const compressedPosition = {
+        line: 1,
+        column: 0
+    };
 
-// const originalPosition = consumer.originalPositionFor(compressedPosition);
-// console.log(originalPosition); 
-
-const fs = require('fs');
-import { SourceMapConsumer } from 'source-map';
-
-// 假设你有一个函数来处理 sourceMap，这里只是一个简单的示例
-function processSourceMap(sourceMap: string) {
-    // 在这个函数中处理 sourceMap
-    SourceMapConsumer.with(sourceMap, null, (consumer: any) => { // 显式声明 consumer 的类型为 any 或者适当的类型
-        // 在这里使用 consumer
-            const transformedPosition = { line: 1, column: 10 };
-
-    // 获取转换后代码的原始位置
-    const originalPosition = consumer.originalPositionFor(transformedPosition);
-
-    // 输出原始位置信息
-    console.log(`Original Position: ${originalPosition.line}:${originalPosition.column}`);
-    });
+    const originalPosition = consumer.originalPositionFor(compressedPosition);
+    console.log(originalPosition); 
 }
 
-// 调用函数并传入 sourceMap
-const sourceMap = fs.readFileSync('D:/11study/openharmony/ArkUIWeChat/resultTsDir/entry/src/main/ets/model/CommonStyle.ts.map', 'utf-8');
-processSourceMap(sourceMap);
+func()
+
+
+// const fs = require('fs');
+// import { SourceMapConsumer } from 'source-map';
+
+// // 假设你有一个函数来处理 sourceMap，这里只是一个简单的示例
+// function processSourceMap(sourceMap: string) {
+//     // 在这个函数中处理 sourceMap
+//     SourceMapConsumer.with(sourceMap, null, (consumer: any) => { // 显式声明 consumer 的类型为 any 或者适当的类型
+//         // 在这里使用 consumer
+//             const transformedPosition = { line: 1, column: 10 };
+
+//     // 获取转换后代码的原始位置
+//     const originalPosition = consumer.originalPositionFor(transformedPosition);
+
+//     // 输出原始位置信息
+//     console.log(`Original Position: ${originalPosition.line}:${originalPosition.column}`);
+//     });
+// }
+
+// // 调用函数并传入 sourceMap
+// const sourceMap = fs.readFileSync('D:/11study/openharmony/ArkUIWeChat/resultTsDir/entry/src/main/ets/model/CommonStyle.ts.map', 'utf-8');
+// processSourceMap(sourceMap);
 
