bbc523739c3e912bdc145a6b39b99aa77105c6b4
huang jianhua <hjh.cn@outlook.com>
2024-03-12 17:16:33 +0800

    add logger sample

--- tests/SceneTest.ts
+++ tests/SceneTest.ts
@@ -1,15 +1,26 @@
+import { SceneConfig } from "../src/Config";
 import { Scene } from "../src/Scene";
-import { SceneConfig } from "./Config";
+import Logger from "../src/utils/logger";
 
-class  SceneTest{
+const logger = Logger.getLogger();
+class SceneTest {
     public buildScene(): Scene {
         // tests\\resources\\scene\\mainModule
-        const config_path = "tests\\resources\\scene\\SceneTestConfig.json";        
+        const config_path = "tests\\resources\\scene\\SceneTestConfig.json";
         let config: SceneConfig = new SceneConfig();
         config.buildFromJson(config_path);
+        // Logger.setLogLevel(LOG_LEVEL.INFO);
         return new Scene(config);
+    }
+
+    public testLogger(): void {
+        let scene = this.buildScene();
+        logger.info('info');
+        logger.warn('info');
+        logger.error('error');
     }
 }
 
 let sceneTest = new SceneTest();
-sceneTest.buildScene();
+// sceneTest.buildScene();
+sceneTest.testLogger();
