f55588e25f312b51728b611bdf7946e4bb6ae34c
19612 <1961289751@qq.com>
2024-03-28 16:21:05 +0800

    ifds div0 case

--- tests/resources/ifds/test.ts
+++ tests/resources/ifds/test.ts
@@ -25,3 +25,42 @@ function  main(x:number, y:number) {
     x = identity(y);
     x = 3/x;
 }
+
+function func1(a:number,b:number){
+    if (a == 0){
+        b = 1;
+        return b;
+    }
+    if (b != 0){
+        return a;
+    }
+    return 0;
+}
+
+function main1(){
+    let x = func1(1,0)
+}
+
+class num {
+    get():number {
+        return -1;
+    }
+}
+
+class one extends num {
+    get(): number {
+        return 1;
+    }
+}
+
+class Zero extends num {
+    get(): number {
+        return 0;
+    }
+}
+
+function main2() {
+    let x = new num().get();
+    x = new one().get();
+    x = new Zero().get();
+}
