17e3a2c034c1e1a297f5abc94e0d1b6d832010bd
code-sunbo <bb.sunbo@huawei.com>
2024-04-23 16:15:34 +0800

    add ViewTree UTest

--- tests/unit/ViewTree.test.ts
+++ tests/unit/ViewTree.test.ts
@@ -3,24 +3,43 @@ import { assert, describe, it, vi,expect } from "vitest";
 import * as tr from "../../src/utils/typeReferenceUtils";
 import { Scene } from "../../src/Scene";
 import path from "path";
-import { ViewTree } from "../../src/core/graph/ViewTree";
 
 let config: SceneConfig = new SceneConfig();
-config.buildFromProjectDir(path.join(__dirname, "../../src/core/graph"));
+config.buildFromProjectDir(path.join(__dirname, "../../tests/resources/viewtree"));
 let scece = new Scene(config);
-let arkFile = tr.getArkFileByName("ViewTree.ts", scece);
-let arkClass = tr.resolveClassInstance("ViewTree.ts.ViewTree", arkFile);
 
-describe("getArkFileByName Test", () => {
-    if (arkClass == null) {
-        assert.isNotNull(arkClass);
-        return;
-    }
-    let arkMethod = arkClass.getMethods()[1];
-    it('true case', () => {
-        let vt: ViewTree = new ViewTree(arkMethod);
-        const spy = vi.spyOn(vt, "isInitialized");
+
+describe("ViewTree Test", () => {
+    it('test state', async () => {
+        let arkFile = tr.getArkFileByName("ControlCenterComponent.ts", scece);
+        let arkClass = tr.resolveClassInstance("ControlCenterComponent.ts.ControlCenterComponent", arkFile);
+        if (arkClass == null) {
+            assert.isNotNull(arkClass);
+            return;
+        }
+        let vt = await arkClass.getViewTree();
         vt.buildViewTree();
-        expect(spy).toHaveBeenCalled();
+        let type = vt.getClassFieldType('mSimpleToggleColumnCount');
+        expect(type).equals('@State');
+
+        let root = vt.getRoot();
+        expect(root.name).equals('Column');
+        expect(root.children[0].children[0].children[0].children[1].children[0].children[0].children[0].name).equals('Grid');
+    })
+
+    it('test if', async () => {
+        let arkFile = tr.getArkFileByName("ParentComponent.ts", scece);
+        let arkClass = tr.resolveClassInstance("ParentComponent.ts.ParentComponent", arkFile);
+        if (arkClass == null) {
+            assert.isNotNull(arkClass);
+            return;
+        }
+        let vt = await arkClass.getViewTree();
+        let type = vt.getClassFieldType('countDownStartValue');
+        expect(type).equals('ObservedPropertySimple');
+
+        let root = vt.getRoot();
+        expect(root.name).equals('Column');
+        expect(root.children[3].children[0].children[0].children[0].name).equals('IfBranch');
     })
 })
