a2b30d5c48b1810c4d24ed374d2b6c3e35263e6d
code-sunbo <bb.sunbo@huawei.com>
2024-04-29 10:32:53 +0800

    add build ets2ts.ts

---
+++ rollup.config.mjs
@@ -0,0 +1,25 @@
+import esbuild from 'rollup-plugin-esbuild';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs'
+
+export default () => [
+    {
+        input: {
+            ets2ts: 'src/utils/Ets2ts.ts'
+        },
+        output: {
+            dir: 'lib',
+            format: 'cjs'
+        },
+        plugins: [
+            resolve(),
+            commonjs({
+                include: /node_modules/
+            }),
+            esbuild({
+                target: 'node18',
+                minify: false
+            })
+        ]
+    }
+]
