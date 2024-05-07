34f372b8175f62482930aaea25e2fa541ca9e5ed
yifei-xue <yifei.van.xue@gmail.com>
2024-05-07 19:15:16 +0800

    improve sdk parsing and support component
    Signed-off-by: yifei-xue <yifei.van.xue@gmail.com>

--- tests/AppTest.ts
+++ tests/AppTest.ts
@@ -2,16 +2,30 @@ import { SceneConfig } from "../src/Config";
 import { Scene } from "../src/Scene";
 
 let config: SceneConfig = new SceneConfig();
+//let config: SceneConfig = new SceneConfig("./tests/AppTestConfigUnix.json");
 
-config.buildFromJson("./tests/AppTestConfig.json");
+
+//config.buildFromJson("./tests/AppTestConfig.json");
 //config.buildFromJson("./tests/sample/AppTestConfig-sample.json");
 //config.buildFromJson("./tests/AppTestConfigUnix.json");
 
 //config.buildFromProjectDir("C:\\msys64\\home\\Yifei\\code\\applications_photos");
 
-//let config: SceneConfig = new SceneConfig("./tests/AppTestConfigUnix.json");
-function runScene(config: SceneConfig) {
+function runScene4Json(config: SceneConfig) {
     let projectScene: Scene = new Scene(config);
     debugger;
 }
-runScene(config);
+async function runScene4Ide() {
+    let config: SceneConfig = new SceneConfig();
+    await config.buildFromIde(
+        "scene",
+        "D:/code/scene_xxx",
+        "C:/msys64/home/xxx/code/yyyy",
+        "D:/code/deveco-studio/sdk/xxx",
+        "C:/mysys64/home/xxx/code/log/xx.log"
+    );
+}
+
+runScene4Json(config);
+
+//runScene4Ide();
