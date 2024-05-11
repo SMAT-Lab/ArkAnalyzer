77d238be5e94b65cf04feaa4c19d59df478a9b96
19612 <1961289751@qq.com>
2024-05-11 12:00:50 +0800

    global variable and decorator

--- tests/resources/type/test.ts
+++ tests/resources/type/test.ts
@@ -1,9 +1,19 @@
-import hilog from '@ohos.hilog';
-
-
-function onCreate(want, launchParam) {
-    hilog.isLoggable(0x0000, 'testTag', hilog.LogLevel.INFO);
-    hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onCreate');
-    hilog.info(0x0000, 'testTag', '%{public}s', 'want param:' + JSON.stringify(want) ?? '');
-    hilog.info(0x0000, 'testTag', '%{public}s', 'launchParam:' + JSON.stringify(launchParam) ?? '');
-}
+function vulnerableFunction(): void {
+      const buffer: string[] = new Array(5).fill(''); // 模拟长度为5的缓冲区
+    
+      const readline = require('readline').createInterface({
+        input: process.stdin,
+        output: process.stdout
+      });
+    
+      readline.question('Enter a value: ', (input: string) => {
+        // 不检查输入长度
+        buffer.length = 0; // 清空缓冲区
+        buffer.push(...input.split('')); // 将输入字符串分割为字符数组,并添加到缓冲区
+    
+        console.log(`You entered: ${buffer.join('')}`);
+        readline.close();
+      });
+    }
+    
+    vulnerableFunction();
