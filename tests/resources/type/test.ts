7f1a324dfe97b41a11f90204fbe898f73a44a699
c-hn <1961289751@qq.com>
2024-05-23 15:47:19 +0800

    cfgBuilder refactor

--- tests/resources/type/test.ts
+++ tests/resources/type/test.ts
@@ -1,19 +1,17 @@
-function vulnerableFunction(): void {
-      const buffer: string[] = new Array(5).fill(''); // 模拟长度为5的缓冲区
-    
-      const readline = require('readline').createInterface({
-        input: process.stdin,
-        output: process.stdout
-      });
-    
-      readline.question('Enter a value: ', (input: string) => {
-        // 不检查输入长度
-        buffer.length = 0; // 清空缓冲区
-        buffer.push(...input.split('')); // 将输入字符串分割为字符数组,并添加到缓冲区
-    
-        console.log(`You entered: ${buffer.join('')}`);
-        readline.close();
-      });
+export class ParticleParameterValue {
+    /**
+     * ParticleParameterValue of a particle parameter.
+     */
+    constructor(value: ParticleParameterValue);
+    constructor(value: number, options: ParticleParameterOptions, name: string);
+    constructor(...args: any[]) {
+        if (args[0] instanceof ParticleParameterValue) {
+            this.Copy(args[0]);
+        }
+        else {
+            this.value = args[0];
+            this.options = args[1];
+            this.name = args[2];
+        }
     }
-    
-    vulnerableFunction();
+}
