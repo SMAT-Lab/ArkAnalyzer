b7e28b84c3d129f6cf6053b4d93edb72c2f6e20d
ZYue001 <1215430211@qq.com>
2025-07-08 15:51:12 +0800

    门禁意见修改
    Signed-off-by: ZYue001 <1215430211@qq.com>

--- src/core_cpp/model/builder/ArkFileBuilder.ts
+++ src/core_cpp/model/builder/ArkFileBuilder.ts
@@ -154,7 +154,7 @@ function buildArkFile(arkFile: ArkFile, astRoot: any): void {
 // Get ArkClass of 'CXXMethodDecl'/'CXXConstructorDecl'/'CXXDestructorDecl'
 function getDeclaringArkClassOfMethod(mtd: any, arkFile: ArkFile): ArkClass {
     const className: string = mtd.mangledName;
-    let arkClass = arkFile.getClasses().find(arkClass => (arkClass.getName() == className));
+    let arkClass = arkFile.getClasses().find(arkClass => (arkClass.getName() === className));
     if (!arkClass) {
         arkClass = new ArkClass();
         const classSignature = new ClassSignature(className, arkFile.getFileSignature());
