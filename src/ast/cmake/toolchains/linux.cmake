68e8a69926faa500fbe34bf8a3cf816e150e82d5
hao <782840540@qq.com>
2025-08-09 10:38:08 +0800

    添加语法树生成工具的构建脚本
    Signed-off-by: hao <782840540@qq.com>

---
+++ src/ast/cmake/toolchains/linux.cmake
@@ -0,0 +1,21 @@
+# --- cmake/toolchains/linux.cmake ---
+
+set(CMAKE_BUILD_WITH_INSTALL_RPATH TRUE)
+set(CMAKE_INSTALL_RPATH_USE_LINK_PATH TRUE)
+set(CMAKE_INSTALL_RPATH "$ORIGIN")
+
+set(CLANG_INCLUDE_DIRS
+   "***//harmony_code/codearts_workspace/pre_scripts/clang+llvm-19.1.7-x86_64-pc-windows-msvc/include"
+   "***//harmony_code/codearts_workspace/pre_scripts/clang+llvm-19.1.7-x86_64-pc-windows-msvc/include/clang-c"
+)
+
+link_directories(
+   "***//harmony_code/codearts_workspace/pre_scripts/clang+llvm-19.1.7-x86_64-pc-windows-msvc/lib"
+)
+
+find_library(CLANG_LIBRARIES
+   NAMES libclang clang clang-cpp
+   PATHS "***//harmony_code/codearts_workspace/pre_scripts/clang+llvm-19.1.7-x86_64-pc-windows-msvc/lib"
+)
+
+message(STATUS "Found libclang on linux: ${CLANG_LIBRARIES}")
