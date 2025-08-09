88cf0aa82b8f9ff30b1cdc84c7848942d40ed423
ZYue001 <1215430211@qq.com>
2025-08-09 16:02:20 +0800

    lambdaFunc用例门禁意见修复&resource_cpp/model无用测试用例删除
    Signed-off-by: ZYue001 <1215430211@qq.com>

--- tests/resources_cpp/cfg/lambdaFunc/lambdaFuncSample.cpp
+++ tests/resources_cpp/cfg/lambdaFunc/lambdaFuncSample.cpp
@@ -16,61 +16,33 @@
 #include <stdio.h>
 
 // 无捕获，有参
-void case1()
+void Case1()
 {
-    auto func = [](int x)
-    { return x + 1; };
+    auto func = [](int x) { return x + 1; };
     int res = func(1);
     printf("%d", res);
 }
 
-// TODO: 可以尝试写一个const变量声明，修改variableDeclarationListToValueAndStmts函数
-
 // 带捕获
-void case2()
+void Case2()
 {
     const int a = 5;
-    auto func = [a](int x)
-    { return x + a; };
-    int res = func(2);
+    auto func = [a](int x) { return x + a; };
+    int res = func(1);
     printf("%d", res);
 }
 
 // 带返回类型
-void case3()
+void Case3()
 {
-    auto func = [](int x) -> int
-    { return x + 1; };
-    int res = func(3);
+    auto func = [](int x) -> int { return x + 1; };
+    int res = func(1);
     printf("%d", res);
 }
 
 // 无返回值
-void case4()
+void Case4()
 {
-    auto func = [](int x)
-    { printf("%d", x); };
-    func(4);
+    auto func = [](int x) { printf("%d", x); };
+    func(1);
 }
-
-// void case5()
-// {
-//     int a = 5;
-//     auto func = [&a](int x) { // 引用传递
-//         a = 6;
-//         return x + a;
-//     };
-//     int res = func(2);
-//     printf("%d", res);
-// }
-
-// void case6()
-// {
-//     int a = 5;
-//     auto func = [a](int x) mutable { // 在函数内可以修改值传递的捕获参数
-//         a = 6;
-//         return x + a;
-//     };
-//     int res = func(2);
-//     printf("%d", res);
-// }
