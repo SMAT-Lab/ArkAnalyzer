327a84bb3f17d3ae8b6d3cab02af1fffc5ee38ef
hao <782840540@qq.com>
2025-08-09 19:14:08 +0800

    更新mignw.cmake
    Signed-off-by: hao <782840540@qq.com>

--- src/ast/cmake/toolchains/mingw.cmake
+++ src/ast/cmake/toolchains/mingw.cmake
@@ -4,7 +4,7 @@
 set(CMAKE_SYSTEM_NAME Windows)
 set(CMAKE_SYSTEM_PROCESSOR x86_64)
 set(CMAKE_C_COMPILER "***//harmony_code/codearts_workspace/pre_scripts/llvm-mingw-x86_64/bin/x86_64-w64-mingw32-clang")
-#set(CMAKE_CXX_COMPILER "***//harmony_code/codearts_workspace/pre_scripts/llvm-mingw-x86_64/bin/x86_64-w64-mingw32-clang++")
+set(CMAKE_CXX_COMPILER "***//harmony_code/codearts_workspace/pre_scripts/llvm-mingw-x86_64/bin/x86_64-w64-mingw32-clang++")
 set(CMAKE_BUILD_WITH_INSTALL_RPATH TRUE)
 set(CMAKE_INSTALL_RPATH_USE_LINK_PATH TRUE)
 set(CMAKE_INSTALL_RPATH "$ORIGIN")
