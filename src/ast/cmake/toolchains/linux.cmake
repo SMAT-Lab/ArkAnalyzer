3d4dbb8c46086216917af25eac5f4249fc49a7da
hao <782840540@qq.com>
2025-08-09 11:16:11 +0800

    添加语法树生成工具的构建脚本
    Signed-off-by: hao <782840540@qq.com>

--- src/ast/cmake/toolchains/linux.cmake
+++ src/ast/cmake/toolchains/linux.cmake
@@ -5,17 +5,17 @@ set(CMAKE_INSTALL_RPATH_USE_LINK_PATH TRUE)
 set(CMAKE_INSTALL_RPATH "$ORIGIN")
 
 set(CLANG_INCLUDE_DIRS
-   "***//harmony_code/codearts_workspace/pre_scripts/clang+llvm-19.1.7-x86_64-pc-windows-msvc/include"
-   "***//harmony_code/codearts_workspace/pre_scripts/clang+llvm-19.1.7-x86_64-pc-windows-msvc/include/clang-c"
+   "***//harmony_code/codearts_workspace/pre_scripts/llvm-19.1.7-x86_64/include"
+   "***//harmony_code/codearts_workspace/pre_scripts/llvm-19.1.7-x86_64/include/clang-c"
 )
 
 link_directories(
-   "***//harmony_code/codearts_workspace/pre_scripts/clang+llvm-19.1.7-x86_64-pc-windows-msvc/lib"
+   "***//harmony_code/codearts_workspace/pre_scripts/llvm-19.1.7-x86_64/lib"
 )
 
 find_library(CLANG_LIBRARIES
    NAMES libclang clang clang-cpp
-   PATHS "***//harmony_code/codearts_workspace/pre_scripts/clang+llvm-19.1.7-x86_64-pc-windows-msvc/lib"
+   PATHS "***//harmony_code/codearts_workspace/pre_scripts/llvm-19.1.7-x86_64/lib"
 )
 
 message(STATUS "Found libclang on linux: ${CLANG_LIBRARIES}")
--- src/ast/cmake/toolchains/mingw.cmake
+++ src/ast/cmake/toolchains/mingw.cmake
@@ -2,7 +2,7 @@
 
 set(CMAKE_SYSTEM_NAME Windows)
 set(CMAKE_SYSTEM_PROCESSOR x86_64)
-set(LLVM_MINGW_ROOT "***//harmony_code/codearts_workspace/pre_scripts/llvm-mingw-20250114-msvcrt-x86_64")
+set(LLVM_MINGW_ROOT "***//harmony_code/codearts_workspace/pre_scripts/llvm-mingw-x86_64")
 set(CMAKE_C_COMPILER "${LLVM_MINGW_ROOT}/bin/x86_64-w64-mingw32-clang")
 set(CMAKE_CXX_COMPILER "${LLVM_MINGW_ROOT}/bin/x86_64-w64-mingw32-clang++")
 set(CMAKE_BUILD_WITH_INSTALL_RPATH TRUE)
@@ -10,14 +10,14 @@ set(CMAKE_INSTALL_RPATH_USE_LINK_PATH TRUE)
 set(CMAKE_INSTALL_RPATH "$ORIGIN")
 
 set(CLANG_INCLUDE_DIRS
-   "***//harmony_code/codearts_workspace/pre_scripts/clang+llvm-19.1.7-x86_64-pc-windows-msvc/include"
-   "***//harmony_code/codearts_workspace/pre_scripts/clang+llvm-19.1.7-x86_64-pc-windows-msvc/include/clang-c"
+   "***//harmony_code/codearts_workspace/pre_scripts/llvm-19.1.7-x86_64/include"
+   "***//harmony_code/codearts_workspace/pre_scripts/llvm-19.1.7-x86_64/clang-c"
 )
 
 link_directories(
-   "***//harmony_code/codearts_workspace/pre_scripts/llvm-mingw-20250114-msvcrt-x86_64/x86_64-w64-mingw32/bin"
+   "***//harmony_code/codearts_workspace/pre_scripts/llvm-mingw-x86_64/x86_64-w64-mingw32/bin"
 )
 
-set(CLANG_LIBRARIES "***//harmony_code/codearts_workspace/pre_scripts/clang+llvm-19.1.7-x86_64-pc-windows-msvc/bin/libclang.dll")
+set(CLANG_LIBRARIES "***//harmony_code/codearts_workspace/pre_scripts/llvm-19.1.7-x86_64/bin/libclang.dll")
 
 message(STATUS "Found libclang on linux: ${CLANG_LIBRARIES}")
