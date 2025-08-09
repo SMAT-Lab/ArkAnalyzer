8171328f4e0b5ba84b579ffb6bebdc88cc90aeb7
hao <782840540@qq.com>
2025-08-09 10:06:16 +0800

    添加语法树生成工具的构建脚本
    Signed-off-by: hao <782840540@qq.com>

---
+++ src/ast/cmake/toolchains/mingw.cmake
@@ -0,0 +1,23 @@
+# --- cmake/toolchains/mingw.cmake ---
+
+set(CMAKE_SYSTEM_NAME Windows)
+set(CMAKE_SYSTEM_PROCESSOR x86_64)
+set(LLVM_MINGW_ROOT "/harmony_code/codearts_workspace/pre_scripts/llvm-mingw-20250114-msvcrt-x86_64")
+set(CMAKE_C_COMPILER "${LLVM_MINGW_ROOT}/bin/x86_64-w64-mingw32-clang")
+set(CMAKE_CXX_COMPILER "${LLVM_MINGW_ROOT}/bin/x86_64-w64-mingw32-clang++")
+set(CMAKE_BUILD_WITH_INSTALL_RPATH TRUE)
+set(CMAKE_INSTALL_RPATH_USE_LINK_PATH TRUE)
+set(CMAKE_INSTALL_RPATH "$ORIGIN")
+
+set(CLANG_INCLUDE_DIRS
+   "/harmony_code/codearts_workspace/pre_scripts/clang+llvm-19.1.7-x86_64-pc-windows-msvc/include"
+   "/harmony_code/codearts_workspace/pre_scripts/clang+llvm-19.1.7-x86_64-pc-windows-msvc/include/clang-c"
+)
+
+link_directories(
+   "/harmony_code/codearts_workspace/pre_scripts/llvm-mingw-20250114-msvcrt-x86_64/x86_64-w64-mingw32/bin"
+)
+
+set(CLANG_LIBRARIES "/harmony_code/codearts_workspace/pre_scripts/clang+llvm-19.1.7-x86_64-pc-windows-msvc/bin/libclang.dll")
+
+message(STATUS "Found libclang on linux: ${CLANG_LIBRARIES}")
