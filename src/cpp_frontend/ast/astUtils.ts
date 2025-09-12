34e841ab5a5e390f236c3e21a81101bcccaac692
hao <782840540@qq.com>
2025-09-12 15:10:48 +0800

    修复门禁测试用例一致
    Signed-off-by: hao <782840540@qq.com>

--- src/cpp_frontend/ast/astUtils.ts
+++ src/cpp_frontend/ast/astUtils.ts
@@ -79,6 +79,9 @@ export class AstUtils {
             logger.info('Parsing completed!');
         }
         let translationUnit = JSON.parse(fs.readFileSync(astPath, 'utf-8')) as CxxAstNode;
+        if (sourceFile.includes('lazyImportCase5')) {
+            console.log(JSON.stringify(translationUnit));
+        }
         translationUnit = this.filter(sourceFile, translationUnit) as CxxAstNode;
         deleteFile(astPath);
         return translationUnit;
