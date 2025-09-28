51decd5b41612743bce49684ff23c357c3aa64ac
qinzihan <2645753614@qq.com>
2025-09-28 10:49:42 +0800

    Modification of ternary expression tempValue filtering issue, Binary Conditional duplicate code issue
    Signed-off-by: qinzihan <2645753614@qq.com>

--- tests/resources_cpp/cfg/binaryConditional/binaryConditionalSampleExpects.ts
+++ tests/resources_cpp/cfg/binaryConditional/binaryConditionalSampleExpects.ts
@@ -23,14 +23,14 @@ export const BINARY_CONDITIONAL_EXPECT_CASE1 = {
                 'j = 0',
                 'k = 1',
                 '%0 = -i',
-                'if %0 != 0',
+                'if %0 != 0'
             ],
             preds: [],
-            succes: [1, 2],
+            succes: [ 1, 2 ]
         },
-        { id: 1, stmts: ['j = -i'], preds: [0], succes: [3] },
-        { id: 2, stmts: ['j = k'], preds: [0], succes: [3] },
-        { id: 3, stmts: ['return j'], preds: [1, 2], succes: [] },
+        { id: 1, stmts: [ 'j = %0' ], preds: [ 0 ], succes: [ 3 ] },
+        { id: 2, stmts: [ 'j = k' ], preds: [ 0 ], succes: [ 3 ] },
+        { id: 3, stmts: [ 'return j' ], preds: [ 1, 2 ], succes: [] }
     ],
 };
 
@@ -43,14 +43,14 @@ export const BINARY_CONDITIONAL_EXPECT_CASE2 = {
                 'a = 0',
                 'b = 1',
                 'c = 2',
-                'if a > b',
+                'if a > b'
             ],
             preds: [],
-            succes: [1, 2],
+            succes: [ 1, 2 ]
         },
-        { id: 1, stmts: ['y = a > b'], preds: [0], succes: [3] },
-        { id: 2, stmts: ['y = c'], preds: [0], succes: [3] },
-        { id: 3, stmts: ['return y'], preds: [1, 2], succes: [] },
+        { id: 1, stmts: [ 'y = a' ], preds: [ 0 ], succes: [ 3 ] },
+        { id: 2, stmts: [ 'y = c' ], preds: [ 0 ], succes: [ 3 ] },
+        { id: 3, stmts: [ 'return y' ], preds: [ 1, 2 ], succes: [] }
     ],
 };
 
@@ -63,14 +63,14 @@ export const BINARY_CONDITIONAL_EXPECT_CASE3 = {
                 'x = 0',
                 'y = 0',
                 'x = x + 1',
-                'if x != 0',
+                'if x != 0'
             ],
             preds: [],
-            succes: [1, 2],
+            succes: [ 1, 2 ]
         },
-        { id: 1, stmts: ['x = x + 1', 'y = x'], preds: [0], succes: [3] },
-        { id: 2, stmts: ['y = 1'], preds: [0], succes: [3] },
-        { id: 3, stmts: ['return y'], preds: [1, 2], succes: [] },
+        { id: 1, stmts: [ 'y = x' ], preds: [ 0 ], succes: [ 3 ] },
+        { id: 2, stmts: [ 'y = 1' ], preds: [ 0 ], succes: [ 3 ] },
+        { id: 3, stmts: [ 'return y' ], preds: [ 1, 2 ], succes: [] }
     ],
 };
 
@@ -83,14 +83,14 @@ export const BINARY_CONDITIONAL_EXPECT_CASE4 = {
                 'x = 0',
                 'y = 0',
                 'x = x + 1',
-                'if x != 0',
+                'if x != 0'
             ],
             preds: [],
-            succes: [1, 2],
+            succes: [ 1, 2 ]
         },
-        { id: 1, stmts: ['x = x + 1', 'y = x'], preds: [0], succes: [3] },
-        { id: 2, stmts: ['y = 1'], preds: [0], succes: [3] },
-        { id: 3, stmts: ['return y'], preds: [1, 2], succes: [] },
+        { id: 1, stmts: [ 'y = x' ], preds: [ 0 ], succes: [ 3 ] },
+        { id: 2, stmts: [ 'y = 1' ], preds: [ 0 ], succes: [ 3 ] },
+        { id: 3, stmts: [ 'return y' ], preds: [ 1, 2 ], succes: [] }
     ],
 };
 
@@ -101,16 +101,17 @@ export const BINARY_CONDITIONAL_EXPECT_CASE5 = {
             stmts: [
                 'this = this: @binaryConditional/binaryConditionalSample.cpp: %dflt',
                 'i = 8',
-                'if i < 0',
+                'if i < 0'
             ],
             preds: [],
-            succes: [1, 2],
+            succes: [ 1, 2 ]
         },
-        { id: 1, stmts: ['j = i < 0'], preds: [0], succes: [5] },
-        { id: 2, stmts: ['if i > 1'], preds: [0], succes: [3, 4] },
-        { id: 3, stmts: ['j = i > 1'], preds: [2], succes: [5] },
-        { id: 4, stmts: ['j = 0'], preds: [2], succes: [5] },
-        { id: 5, stmts: ['return j'], preds: [1, 3, 4], succes: [] },
+        { id: 1, stmts: [ 'j = i' ], preds: [ 0 ], succes: [ 6 ] },
+        { id: 2, stmts: [ 'if i > 1' ], preds: [ 0 ], succes: [ 3, 4 ] },
+        { id: 3, stmts: [ '%1 = i' ], preds: [ 2 ], succes: [ 5 ] },
+        { id: 4, stmts: [ '%1 = 0' ], preds: [ 2 ], succes: [ 5 ] },
+        { id: 5, stmts: [ 'j = %1' ], preds: [ 3, 4 ], succes: [ 6 ] },
+        { id: 6, stmts: [ 'return j' ], preds: [ 1, 5 ], succes: [] }
     ],
 };
 
@@ -126,18 +127,13 @@ export const BINARY_CONDITIONAL_EXPECT_CASE6 = {
                 '%0 = i + j',
                 '%1 = %0 + k',
                 '%2 = %1 + 1',
-                'if %2 != 0',
+                'if %2 != 0'
             ],
             preds: [],
-            succes: [1, 2],
+            succes: [ 1, 2 ]
         },
-        {
-            id: 1,
-            stmts: ['%3 = i + j', '%4 = %3 + k', 'j = %4 + 1'],
-            preds: [0],
-            succes: [3],
-        },
-        { id: 2, stmts: ['j = k'], preds: [0], succes: [3] },
-        { id: 3, stmts: ['return j'], preds: [1, 2], succes: [] },
+        { id: 1, stmts: [ 'j = %2' ], preds: [ 0 ], succes: [ 3 ] },
+        { id: 2, stmts: [ 'j = k' ], preds: [ 0 ], succes: [ 3 ] },
+        { id: 3, stmts: [ 'return j' ], preds: [ 1, 2 ], succes: [] }
     ],
 };
