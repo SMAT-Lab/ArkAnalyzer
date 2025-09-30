0a44b86aaaf64dd9adf7c9e8274849e5b32ffe1d
qinzihan <2645753614@qq.com>
2025-09-30 17:52:10 +0800

    Modify use cases
    Signed-off-by: qinzihan <2645753614@qq.com>

--- tests/resources_cpp/cfg/builtInAndSTLFunc/builtInAndSTLFunction.cpp
+++ tests/resources_cpp/cfg/builtInAndSTLFunc/builtInAndSTLFunction.cpp
@@ -38,7 +38,6 @@ void CXXTypeidExprTest()
 void ArrayTypeTraitTest()
 {
     int arr2[5][3];
-    decltype(arr2) arr2Type;
     int rank2 = __array_rank(decltype(arr2));
     int dim1Size = __array_extent(decltype(arr2), 1);
 }
