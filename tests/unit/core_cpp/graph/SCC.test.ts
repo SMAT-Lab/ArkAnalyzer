f7b3b65cb55db91f8c923a23c1fe6341dc8adf24
qinzihan <2645753614@qq.com>
2025-10-13 21:12:21 +0800

    refactor scc ut
    Signed-off-by: Gwatermelon <772422673@qq.com>

--- tests/unit/core_cpp/graph/SCC.test.ts
+++ tests/unit/core_cpp/graph/SCC.test.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -18,7 +18,7 @@ import { describe, expect, it } from 'vitest';
 
 describe('SCCTest', () => {
     let config: SceneConfig = new SceneConfig();
-    config.buildFromProjectDir('./tests/resources/scc');
+    config.buildFromProjectDir('./tests/resources_cpp/scc');
     let projectScene: Scene = new Scene();
     projectScene.buildSceneFromProjectDir(config);
     projectScene.inferTypes();
