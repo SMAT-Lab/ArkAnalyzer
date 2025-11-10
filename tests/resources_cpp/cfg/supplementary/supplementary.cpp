fa1a5455dddfa8f03ea9fc3af7145951b3db7847
qinzihan <2645753614@qq.com>
2025-11-10 17:23:39 +0800

    Add post++testing code
    Signed-off-by: qinzihan <2645753614@qq.com>

--- tests/resources_cpp/cfg/supplementary/supplementary.cpp
+++ tests/resources_cpp/cfg/supplementary/supplementary.cpp
@@ -17,3 +17,46 @@ void Case1()
 {
     int a[10] = {0};
 }
+
+void postAdd()
+{
+    int a = 0;
+    // 1.Auto-increment after use
+    int b = a++;
+    // 2.Post-increment after complex expressions
+    int c = a + a++;
+
+    int d = 0;
+    // 3.When not declared, use auto increment
+    d = b++;
+    d = c++ * b++;
+
+    // 4.Only self increasing, without affecting other variables
+    for (int i = 0; i < 4; i++)
+    {
+    }
+
+    // 5.Multivariate parallel self increasing
+    for (int i = 0,j = 1; i < 4; i++,j++)
+    {
+    }
+
+    // 6.If is judged as an expression and then self increases, affecting the distribution of branches
+    if (a++ > 0)
+    {
+        b = 2;
+    }
+    while (a++ > 0){}
+    do{} while (a++ > 0);
+
+    // 7.As an expression in ternary expressions, it increases after judgment and affects the distribution of branches
+    int f = a++ > 0 ? 1 : 3;
+        int e = a++?: 3; // Default ternary expression
+    // 8.The use of comma operator in linking
+    int g = (a++ ,a + 1, a + 2);
+
+    // 9.The array increases automatically after use
+    int arr[10] = {0};
+    int idx = 1;
+    arr[idx++] = 1;
+}
