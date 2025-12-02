4673d8e395f5cad9770d3254666cfe3d3f6e2aff
ZYue001 <1215430211@qq.com>
2025-12-02 15:05:17 +0800

    Unit Tests for C++ Type Deduction
    Signed-off-by: ZYue001 <1215430211@qq.com>

---
+++ tests/resources_cpp/exports/crossFileCase/expectedIR.ts
@@ -0,0 +1,107 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { ExportType } from '../../../../src/core/model/ArkExport';
+import { ArkMethod, DEFAULT_ARK_CLASS_NAME, ArkClass, ArkNamespace } from '../../../../src';
+
+export const MY_HEADER_EXPORT_INFO1 = {
+    _default: false,
+    exportClauseName: 'Color',
+    exportClauseType: ExportType.CLASS,
+    modifiers: [],
+    arkExport: {
+        type: ArkClass,
+        classSignature: `@exports/crossFileCase/include/myHeader.h: Color`,
+        modifiers: [],
+    },
+};
+
+export const MY_HEADER_EXPORT_INFO2 = {
+    _default: false,
+    exportClauseName: 'FuncDoSomething',
+    exportClauseType: ExportType.METHOD,
+    modifiers: [],
+    arkExport: {
+        type: ArkMethod,
+        methodSignature: `@exports/crossFileCase/src/myHeader.cpp: ${DEFAULT_ARK_CLASS_NAME}.FuncDoSomething(int, int)`,
+        methodDeclareSignature: `@exports/crossFileCase/include/myHeader.h: ${DEFAULT_ARK_CLASS_NAME}.FuncDoSomething(int, int)`,
+        modifiers: [],
+    },
+};
+
+export const MY_HEADER_EXPORT_INFO3 = {
+    _default: false,
+    exportClauseName: 'Circle',
+    exportClauseType: ExportType.CLASS,
+    modifiers: [],
+    arkExport: {
+        type: ArkClass,
+        classSignature: `@exports/crossFileCase/include/myHeader.h: Circle`,
+        modifiers: [],
+    },
+};
+
+export const MY_HEADER_EXPORT_INFO4 = {
+    _default: false,
+    exportClauseName: 'nsA',
+    exportClauseType: ExportType.NAME_SPACE,
+    modifiers: [],
+    arkExport: {
+        type: ArkNamespace,
+        namespaceSignature: `@exports/crossFileCase/include/namespace.h: nsA`,
+        modifiers: [],
+    },
+};
+
+export const NAMESPACE_EXPORT_INFO = {
+    _default: false,
+    exportClauseName: 'nsA',
+    exportClauseType: ExportType.NAME_SPACE,
+    modifiers: [],
+    arkExport: {
+        type: ArkNamespace,
+        namespaceSignature: `@exports/crossFileCase/include/namespace.h: nsA`,
+        modifiers: [],
+    },
+};
+
+export const MAIN_CASE = {
+    blocks: [
+        {
+            id: 0,
+            stmts: [
+                'this = this: @exports/crossFileCase/main.cpp: %dflt',
+                '%0 = new @exports/crossFileCase/include/namespace.h: nsA.DefaultClass',
+                'instanceinvoke %0.<@exports/crossFileCase/include/namespace.h: nsA.DefaultClass.constructor(char, int)>(A, 18)',
+                'dClass = %0',
+                'instanceinvoke dClass.<@exports/crossFileCase/include/namespace.h: nsA.DefaultClass.PrintInfo()>()',
+                'staticinvoke <@exports/crossFileCase/include/namespace.h: nsA.%dflt.FuncInNamespace()>()',
+                '%1 = new @exports/crossFileCase/include/namespace.h: nsA.Point',
+                'instanceinvoke %1.<@exports/crossFileCase/include/namespace.h: nsA.Point.constructor()>(1, 2)',
+                'p = %1',
+                '%2 = new @exports/crossFileCase/include/myHeader.h: Circle',
+                '%3 = @exports/crossFileCase/include/myHeader.h: Color.[static]GREEN',
+                'instanceinvoke %2.<@exports/crossFileCase/include/myHeader.h: Circle.constructor(@exports/crossFileCase/include/namespace.h: nsA.Point&, double, @exports/crossFileCase/include/myHeader.h: Color)>(p, 1, %3)',
+                'c = %2',
+                'area = instanceinvoke c.<@exports/crossFileCase/include/myHeader.h: Circle.CalculateArea()>()',
+                "staticinvoke <@%unk/%unk: .cout()>('Area of circle is : ', area)",
+                'instanceinvoke c.<@exports/crossFileCase/include/myHeader.h: Circle.PrintInfo()>()',
+                'return 0'
+            ],
+            preds: [],
+            succes: [],
+        },
+    ],
+};
---
+++ tests/unit/core_cpp/export/ExportInfo.test.ts
@@ -0,0 +1,307 @@
+/*
+ * Copyright (c) 2024-2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { assert, describe, it } from 'vitest';
+import path from 'path';
+import { ArkClass, FileSignature, Local, Scene, SceneConfig, ArkNamespace, ArkAssignStmt } from '../../../../src';
+import { ArkExport, ExportInfo } from '../../../../src/core/model/ArkExport';
+import { ArkBaseModel, ModifierType } from '../../../../src/core/model/ArkBaseModel';
+import { CAST_SAMPLE_EXPORT_INFO_EXPECT_IR, MY_HEADER_EXPORT_INFO_EXPECT_IR } from '../../../resources_cpp/exports/indirectRef/expectedIR';
+import { ArkMethod } from '../../../../src';
+import {
+    MAIN_CASE,
+    MY_HEADER_EXPORT_INFO1,
+    MY_HEADER_EXPORT_INFO2,
+    MY_HEADER_EXPORT_INFO3,
+    MY_HEADER_EXPORT_INFO4,
+    NAMESPACE_EXPORT_INFO,
+} from '../../../resources_cpp/exports/crossFileCase/expectedIR';
+import { assertBlocksEqual } from '../../common';
+
+const BASE_DIR = 'tests/resources_cpp/exports';
+const is_system_win32 = process.platform === 'win32';
+
+function buildScene(folderName: string): Scene {
+    let config: SceneConfig = new SceneConfig();
+    config.buildFromProjectDir(path.join(BASE_DIR, folderName));
+    let projectScene: Scene = new Scene();
+    projectScene.buildSceneFromProjectDir(config);
+    projectScene.inferTypes();
+    return projectScene;
+}
+
+function compareModifiers(arkModel: ArkBaseModel | ArkExport, expectedModifiers: any): void {
+    if (expectedModifiers.includes('EXPORT')) {
+        assert.isTrue(arkModel.containsModifier(ModifierType.EXPORT));
+    } else {
+        assert.isFalse(arkModel.containsModifier(ModifierType.EXPORT));
+    }
+    if (expectedModifiers.includes('DEFAULT')) {
+        assert.isTrue(arkModel.containsModifier(ModifierType.DEFAULT));
+    } else {
+        assert.isFalse(arkModel.containsModifier(ModifierType.DEFAULT));
+    }
+}
+
+function compareExportInfo(exportInfo: ExportInfo | undefined, expectIR: any): void {
+    assert.isDefined(exportInfo);
+    assert.equal(exportInfo!.isDefault(), expectIR._default);
+    assert.equal(exportInfo!.getExportClauseType(), expectIR.exportClauseType);
+    if (expectIR.modifiers !== undefined) {
+        compareModifiers(exportInfo as ExportInfo, expectIR.modifiers);
+    }
+
+    const arkExport = exportInfo!.getArkExport();
+    assert.isDefined(arkExport);
+    assert.isNotNull(arkExport);
+    assert.isTrue(arkExport instanceof expectIR.arkExport.type);
+    if (expectIR.arkExport.type === ArkClass) {
+        assert.equal((arkExport as ArkClass).getSignature().toString(), expectIR.arkExport.classSignature);
+        if (expectIR.arkExport.classDeclareSignature) {
+            assert.equal((arkExport as ArkClass).getDeclaringArkFile().toString(), expectIR.arkExport.classDeclareSignature);
+        }
+        if (expectIR.arkExport.modifiers !== undefined) {
+            compareModifiers(arkExport as ArkExport, expectIR.arkExport.modifiers);
+        }
+    } else if (expectIR.arkExport.type === Local) {
+        if (expectIR.arkExport.local.type === ArkClass) {
+            assert.equal((arkExport as Local).getType().toString(), expectIR.arkExport.local.classSignature);
+        }
+    } else if (expectIR.arkExport.type === ArkMethod) {
+        assert.equal((arkExport as ArkMethod).getDeclareSignatures()?.[0].toString(), expectIR.arkExport.methodDeclareSignature);
+        assert.equal((arkExport as ArkMethod).getSignature().toString(), expectIR.arkExport.methodSignature);
+        if (expectIR.arkExport.modifiers !== undefined) {
+            compareModifiers(arkExport as ArkExport, expectIR.arkExport.modifiers);
+        }
+    } else if (expectIR.arkExport.type === ArkNamespace) {
+        assert.equal((arkExport! as ArkNamespace).getSignature().toString(), expectIR.arkExport.namespaceSignature);
+    }
+}
+
+let projectScene = buildScene('');
+
+describe('export Test', () => {
+    it('function implement in header file case1', () => {
+        const fileId1 = new FileSignature(projectScene.getProjectName(), 'funcImplementInHeaderFile/sameDir/main.cpp');
+        const file1 = projectScene.getFile(fileId1);
+        assert.equal(file1?.getExportInfos().length, 0);
+        assert.equal(file1?.getImportInfos().length, 3);
+        const stmts = file1?.getDefaultClass().getMethodWithName('main')?.getCfg()?.getStmts();
+        assert.isNotEmpty(stmts);
+        if (stmts) {
+            assert.equal(
+                stmts[1].getInvokeExpr()?.getMethodSignature().toString(),
+                '@exports/funcImplementInHeaderFile/sameDir/myHeader.h: %dflt.FuncDoSomething(int, int)'
+            );
+        }
+
+        const fileId2 = new FileSignature(projectScene.getProjectName(), 'funcImplementInHeaderFile/sameDir/myHeader.h');
+        const file2 = projectScene.getFile(fileId2);
+        assert.equal(file2?.getExportInfos().length, 1);
+        assert.equal(file2?.getImportInfos().length, 0);
+        const stmts2 = file2?.getDefaultClass().getMethodWithName('FuncDoSomething')?.getCfg()?.getStmts();
+        assert.isNotEmpty(stmts2);
+    });
+
+    it('function implement in header file case2', () => {
+        const fileId1 = new FileSignature(projectScene.getProjectName(), 'funcImplementInHeaderFile/diffDir/main.cpp');
+        const file1 = projectScene.getFile(fileId1);
+        assert.equal(file1?.getExportInfos().length, 0);
+        assert.equal(file1?.getImportInfos().length, 3);
+        const stmts = file1?.getDefaultClass().getMethodWithName('main')?.getCfg()?.getStmts();
+        assert.isNotEmpty(stmts);
+        if (stmts) {
+            assert.equal(
+                stmts[1].getInvokeExpr()?.getMethodSignature().toString(),
+                '@exports/funcImplementInHeaderFile/diffDir/include/myHeader.h: %dflt.FuncDoSomething(int, int)'
+            );
+        }
+
+        const fileId2 = new FileSignature(projectScene.getProjectName(), 'funcImplementInHeaderFile/diffDir/include/myHeader.h');
+        const file2 = projectScene.getFile(fileId2);
+        assert.equal(file2?.getExportInfos().length, 1);
+        assert.equal(file2?.getImportInfos().length, 0);
+        const stmts2 = file2?.getDefaultClass().getMethodWithName('FuncDoSomething')?.getCfg()?.getStmts();
+        assert.isNotEmpty(stmts2);
+    });
+
+    it('function implement in cpp file case', () => {
+        const fileId1 = new FileSignature(projectScene.getProjectName(), 'funcImplementInCpp/main.cpp');
+        const file1 = projectScene.getFile(fileId1);
+        assert.equal(file1?.getExportInfos().length, 0);
+        assert.equal(file1?.getImportInfos().length, 6);
+        const stmts = file1?.getDefaultClass().getMethodWithName('main')?.getCfg()?.getStmts();
+        assert.isNotEmpty(stmts);
+        if (stmts && is_system_win32) {
+            assert.equal(
+                stmts[1].getInvokeExpr()?.getMethodSignature().toString(),
+                '@exports/funcImplementInCpp/include/test.h: %dflt.FuncDoSomething(int, int)'
+            );
+            assert.equal(stmts[9].getDef()?.getType().toString(), '@exports/funcImplementInCpp/include/test.h: Circle');
+            assert.equal(stmts[10].getInvokeExpr()?.getMethodSignature().toString(), '@exports/funcImplementInCpp/include/test.h: Circle.CalculateArea()');
+            assert.equal(stmts[12].getInvokeExpr()?.getMethodSignature().toString(), '@exports/funcImplementInCpp/include/test.h: Circle.PrintInfo()');
+        }
+
+        const fileId2 = new FileSignature(projectScene.getProjectName(), 'funcImplementInCpp/include/test.h');
+        const file2 = projectScene.getFile(fileId2);
+        assert.equal(file2?.getExportInfos().length, 4);
+        assert.equal(file2?.getImportInfos().length, 0);
+        const stmts2 = file2?.getDefaultClass().getMethodWithName('FuncDoSomething')?.getCfg()?.getStmts();
+        assert.isUndefined(stmts2);
+
+        const fileId3 = new FileSignature(projectScene.getProjectName(), 'funcImplementInCpp/src/test.cpp');
+        const file3 = projectScene.getFile(fileId3);
+        assert.equal(file3?.getExportInfos().length, 0);
+        assert.equal(file3?.getImportInfos().length, 0);
+        const stmts3 = file3?.getClassWithName('Circle')?.getMethodWithName('CalculateArea')?.getCfg()?.getStmts();
+        assert.isNotEmpty(stmts3);
+        assert.equal(stmts3![1].toString(), '%0 = this.<@exports/funcImplementInCpp/src/test.cpp: Circle.radius>');
+    });
+
+    it('Indirect referencing header file case', () => {
+        const fileId1 = new FileSignature(projectScene.getProjectName(), 'indirectRef/main.cpp');
+        const file1 = projectScene.getFile(fileId1);
+        const stmts = file1?.getDefaultClass().getMethodWithName('main')?.getCfg()?.getStmts();
+        assert.isNotEmpty(stmts);
+        if (stmts) {
+            assert.equal(stmts[1].getInvokeExpr()?.getMethodSignature().toString(), '@exports/indirectRef/include/myHeader.h: %dflt.FuncDoSomething(int, int)');
+        }
+        let importInfos = file1?.getImportInfos();
+        assert.equal(importInfos!.length, 5);
+        assert.equal(importInfos![0].getLazyExportInfo()?.getArkExport()?.getSignature().toString(), '@exports/indirectRef/include/myHeader.h: %dflt');
+        assert.equal(importInfos![3].getLazyExportInfo()?.getArkExport()?.getSignature().toString(), '@exports/indirectRef/include/castSample.h: %dflt');
+        let invokeMethod = importInfos![4].getLazyExportInfo()?.getArkExport();
+        assert.isNotEmpty(invokeMethod);
+        assert.isTrue(invokeMethod instanceof ArkMethod);
+        assert.equal(
+            (invokeMethod as ArkMethod).getSignature().toString(),
+            '@exports/indirectRef/src/castSample.cpp: %dflt.CXXStaticCast(int)'
+        );
+        assert.equal(
+            (invokeMethod as ArkMethod).getDeclareSignatures()?.[0].toString(),
+            '@exports/indirectRef/include/castSample.h: %dflt.CXXStaticCast(int)'
+        );
+
+        const fileId2 = new FileSignature(projectScene.getProjectName(), 'indirectRef/include/myHeader.h');
+        const file2 = projectScene.getFile(fileId2);
+        assert.equal(file2?.getExportInfos().length, 1);
+        let exportInfo = file2?.getExportInfoBy(MY_HEADER_EXPORT_INFO_EXPECT_IR.exportClauseName);
+        compareExportInfo(exportInfo, MY_HEADER_EXPORT_INFO_EXPECT_IR);
+        importInfos = file2?.getImportInfos();
+        assert.equal(file2?.getImportInfos().length, 2);
+        assert.equal(importInfos![0].getLazyExportInfo()?.getArkExport()?.getSignature().toString(), '@exports/indirectRef/include/castSample.h: %dflt');
+        invokeMethod = importInfos![1].getLazyExportInfo()?.getArkExport();
+        assert.isNotEmpty(invokeMethod);
+        assert.isTrue(invokeMethod instanceof ArkMethod);
+        assert.equal(
+            (invokeMethod as ArkMethod).getSignature().toString(),
+            '@exports/indirectRef/src/castSample.cpp: %dflt.CXXStaticCast(int)'
+        );
+        assert.equal(
+            (invokeMethod as ArkMethod).getDeclareSignatures()?.[0].toString(),
+            '@exports/indirectRef/include/castSample.h: %dflt.CXXStaticCast(int)'
+        );
+
+        const fileId3 = new FileSignature(projectScene.getProjectName(), 'indirectRef/include/castSample.h');
+        const file3 = projectScene.getFile(fileId3);
+        assert.equal(file3?.getExportInfos().length, 1);
+        assert.equal(file3?.getImportInfos().length, 0);
+        exportInfo = file3?.getExportInfoBy(CAST_SAMPLE_EXPORT_INFO_EXPECT_IR.exportClauseName);
+        compareExportInfo(exportInfo, CAST_SAMPLE_EXPORT_INFO_EXPECT_IR);
+    });
+
+    it('cross file case1', () => {
+        const fileId = new FileSignature(projectScene.getProjectName(), 'crossFileCase/include/myHeader.h');
+        const file = projectScene.getFile(fileId);
+        assert.equal(file?.getExportInfos().length, 4);
+        let exportInfo: ExportInfo | undefined;
+        exportInfo = file?.getExportInfoBy(MY_HEADER_EXPORT_INFO1.exportClauseName);
+        compareExportInfo(exportInfo, MY_HEADER_EXPORT_INFO1);
+        exportInfo = file?.getExportInfoBy(MY_HEADER_EXPORT_INFO2.exportClauseName);
+        compareExportInfo(exportInfo, MY_HEADER_EXPORT_INFO2);
+        exportInfo = file?.getExportInfoBy(MY_HEADER_EXPORT_INFO3.exportClauseName);
+        assert.isNotEmpty(exportInfo);
+        compareExportInfo(exportInfo, MY_HEADER_EXPORT_INFO3);
+        assert.equal(
+            (exportInfo?.getArkExport() as ArkClass).getFieldWithName('center')?.getType().toString(),
+            '@exports/crossFileCase/include/namespace.h: nsA.Point'
+        );
+        assert.equal(
+            (exportInfo?.getArkExport() as ArkClass).getMethodWithName('CalculateArea')?.getSignature().toString(),
+            '@exports/crossFileCase/src/myHeader.cpp: Circle.CalculateArea()'
+        );
+        exportInfo = file?.getExportInfoBy(MY_HEADER_EXPORT_INFO4.exportClauseName);
+        compareExportInfo(exportInfo, MY_HEADER_EXPORT_INFO4);
+    });
+
+    it('cross file case2', () => {
+        const fileId = new FileSignature(projectScene.getProjectName(), 'crossFileCase/src/myHeader.cpp');
+        const file = projectScene.getFile(fileId);
+        assert.equal(file?.getImportInfos().length, 6);
+        assert.equal(
+            file?.getDefaultClass().getMethodWithName('FuncDoSomething')?.getDeclareSignatures()?.[0].toString(),
+            '@exports/crossFileCase/include/myHeader.h: %dflt.FuncDoSomething(int, int)'
+        );
+        assert.equal(
+            file?.getClassWithName('Circle')?.getDeclareSignature()?.toString(),
+            '@exports/crossFileCase/include/myHeader.h: Circle'
+        );
+        const stmts = file?.getClassWithName('Circle')?.getMethodWithName('PrintInfo')?.getCfg()?.getStmts();
+        assert.isNotEmpty(stmts);
+        assert.equal(stmts![1].toString(), '%0 = this.<@exports/crossFileCase/include/myHeader.h: Circle.center>');
+        assert.equal(stmts![2].toString(), '%1 = %0.<@exports/crossFileCase/include/namespace.h: nsA.Point.x>');
+        assert.equal(stmts![3].toString(), '%2 = this.<@exports/crossFileCase/include/myHeader.h: Circle.center>');
+        assert.equal(stmts![4].toString(), '%3 = %2.<@exports/crossFileCase/include/namespace.h: nsA.Point.y>');
+    });
+
+    it('cross file case3', () => {
+        const fileId = new FileSignature(projectScene.getProjectName(), 'crossFileCase/include/namespace.h');
+        const file = projectScene.getFile(fileId);
+        assert.equal(file?.getExportInfos().length, 1);
+        let exportInfo: ExportInfo | undefined;
+        exportInfo = file?.getExportInfoBy(NAMESPACE_EXPORT_INFO.exportClauseName);
+        compareExportInfo(exportInfo, NAMESPACE_EXPORT_INFO);
+    });
+
+    it('cross file case4', () => {
+        const fileId = new FileSignature(projectScene.getProjectName(), 'crossFileCase/src/namespace.cpp');
+        const file = projectScene.getFile(fileId);
+        assert.equal(file?.getImportInfos().length, 3);
+        assert.equal(
+            file?.getNamespaceWithName('nsA')?.getDefaultClass().getMethodWithName('FuncInNamespace')?.
+            getDeclareSignatures()?.[0].toString(),
+            '@exports/crossFileCase/include/namespace.h: nsA.%dflt.FuncInNamespace()'
+        );
+        assert.equal(
+            file?.getNamespaceWithName('nsA')?.getClassWithName('DefaultClass')?.getDeclareSignature()?.toString(),
+            '@exports/crossFileCase/include/namespace.h: nsA.DefaultClass'
+        );
+        const defaultClass = file?.getNamespaceWithName('nsA')?.getClassWithName('DefaultClass');
+        const stmts = defaultClass?.getMethodWithName('constructor')?.getCfg()?.getStmts();
+        assert.isNotEmpty(stmts);
+        assert.equal((stmts![3] as ArkAssignStmt)!.getLeftOp().getType().toString(), 'char');
+        assert.equal((stmts![4] as ArkAssignStmt)!.getLeftOp().getType().toString(), 'int');
+    });
+
+    it('cross file case5', () => {
+        const fileId = new FileSignature(projectScene.getProjectName(), 'crossFileCase/main.cpp');
+        const file = projectScene.getFile(fileId);
+        assert.equal(file?.getImportInfos().length, 6);
+        const blocks = file?.getDefaultClass().getMethodWithName('main')?.getCfg()?.getBlocks();
+        assert.isDefined(blocks);
+        assertBlocksEqual(blocks!, MAIN_CASE.blocks);
+    });
+
+});
