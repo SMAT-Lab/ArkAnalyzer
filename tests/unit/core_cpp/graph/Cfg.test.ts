042fb755aba3f701c72604b222ccc5b75132fa66
qinzihan <2645753614@qq.com>
2025-12-02 09:56:33 +0800

    modify for access control
    Signed-off-by: qinzihan <2645753614@qq.com>

--- tests/unit/core_cpp/graph/Cfg.test.ts
+++ tests/unit/core_cpp/graph/Cfg.test.ts
@@ -575,7 +575,7 @@ function testNamespaceClasses(scene: Scene, filePath: string, namespaceName: str
         throw new Error(`Namespace ${namespaceName} not found in file ${filePath}`);
     }
 
-    const namespaceBlockMap = new Map<string, unknown>();
+    const namespaceBlockMap = new Map<string, any>();
     for (const classBlock of expectBlocks) {
         namespaceBlockMap.set(classBlock.className, classBlock);
     }
