ecbdc82b0fcf63e9e27c966d0bd84bf31c220063
qinzihan <2645753614@qq.com>
2025-12-03 14:08:19 +0800

    modify for access
    Signed-off-by: qinzihan <2645753614@qq.com>

--- tests/resources_cpp/cfg/goto/gotoSampleExpects.ts
+++ tests/resources_cpp/cfg/goto/gotoSampleExpects.ts
@@ -199,13 +199,13 @@ export const GOTO_EXPECT_CASE11 = {
             stmts: [
                 'this = this: @goto/gotoSample.cpp: %dflt',
                 'b = undefined',
-                'c = undefined'
+                'c = undefined',
             ],
             preds: [],
-            succes: [1]
+            succes: [1],
         },
         { id: 1, stmts: ['c = 2'], preds: [0], succes: [2] },
-        { id: 2, stmts: ['c = c + 1', 'return'], preds: [1], succes: [] }
+        { id: 2, stmts: ['c = c + 1', 'return'], preds: [1], succes: [] },
     ],
 };
 
@@ -216,13 +216,13 @@ export const GOTO_EXPECT_CASE12 = {
             stmts: [
                 'this = this: @goto/gotoSample.cpp: %dflt',
                 'a = undefined',
-                'c = undefined'
+                'c = undefined',
             ],
             preds: [],
-            succes: [1]
+            succes: [1],
         },
         { id: 1, stmts: ['c = 2'], preds: [0], succes: [2] },
         { id: 2, stmts: ['c = c + 1'], preds: [1], succes: [3] },
-        { id: 3, stmts: ['return c'], preds: [2], succes: [] }
+        { id: 3, stmts: ['return c'], preds: [2], succes: [] },
     ],
 };
