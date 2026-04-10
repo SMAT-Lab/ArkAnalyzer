e90e06488200c2acf9a2f692e7eccceffc6828e6
Ji Xuye <jixuye@huawei.com>
2026-04-10 09:10:30 +0800

    fix ci Signed-off-by: Ji Xuye <jixuye@huawei.com>
    Signed-off-by: Ji Xuye <jixuye@huawei.com>

--- script/vendorOhosTypescript.js
+++ script/vendorOhosTypescript.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024-2025 Huawei Device Co., Ltd.
+ * Copyright (c) 2024-2026 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -27,7 +27,7 @@ if (!fs.existsSync(sourceDir)) {
     process.exit(1);
 }
 
-if (!fs.existsSync(markerPath)) {
+if (!process.env.CLOUD_BUILD_ENV && !fs.existsSync(markerPath)) {
     console.error('[vendorOhosTypescript] marker missing in source:', markerPath);
     process.exit(1);
 }
